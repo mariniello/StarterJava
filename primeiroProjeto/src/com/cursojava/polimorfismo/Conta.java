713d06e03eaf91fd2cf42f852d56d0bc279a727e
mariniello <mariniello@hotmail.com>
2019-11-13 09:07:17 -0300

    Commit : Poliformismo

---
+++ primeiroProjeto/src/com/cursojava/polimorfismo/Conta.java
@@ -0,0 +1,14 @@
+package com.cursojava.polimorfismo;
+
+public class Conta {
+
+
+	protected double saldo;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
