5223d62032520a9c4e9227c21dab719fb08b91cf
mariniello <mariniello@hotmail.com>
2019-11-13 09:08:48 -0300

    Commit : Classes Abstratas

---
+++ primeiroProjeto/src/com/cursojava/ClasseAbstrata/Produto.java
@@ -0,0 +1,12 @@
+package com.cursojava.ClasseAbstrata;
+
+public abstract class Produto {
+
+String descricao;
+	
+	public abstract void imprimirDescricao();
+	
+	public String getDescricao() {
+		return descricao;
+	}
+}
