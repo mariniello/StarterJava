ed2b1ff14b1d892bf8422865e30cf20605cff6f5
mariniello <mariniello@hotmail.com>
2019-11-22 15:27:24 -0300

    Ordenadores em JAVA (Comparate)

---
+++ primeiroProjeto/src/cursoJava/ordenador/OrdenandoPelaIdade.java
@@ -0,0 +1,30 @@
+package cursoJava.ordenador;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenandoPelaIdade {
+
+	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa("João", 22);
+		Pessoa p2 = new Pessoa("Adriana", 23);
+		Pessoa p3 = new Pessoa("Bruno", 21);
+		
+		List<Pessoa> pessoas = Arrays.asList(p1, p2, p3);
+		
+		IdadeComparator idadeComparator = new IdadeComparator();
+		
+		Collections.sort(pessoas, idadeComparator );	
+		
+		for(Pessoa pessoa : pessoas) {
+			System.out.println(pessoa);
+		}
+		
+		//JAVA 8
+		pessoas.sort(idadeComparator);
+		pessoas.forEach(System.out::println);
+		
+	}
+}
