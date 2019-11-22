2f7e20737159658e79eb886b89d746e10efb4604
mariniello <mariniello@hotmail.com>
2019-11-22 17:09:21 -0300

    Collections(Listas, Map) em Java

---
+++ primeiroProjeto/src/cursoJava/Collections/Cargo.java
@@ -0,0 +1,26 @@
+package cursoJava.Collections;
+
+import java.math.BigDecimal;
+
+public class Cargo {
+
+	private String descricao;
+	private BigDecimal salario;
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public BigDecimal getSalario() {
+		return this.salario;
+	}
+	
+	public void setSalario(BigDecimal salario) {
+		this.salario = salario;
+	}
+	
+}
